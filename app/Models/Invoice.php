560b308d4e96dd58198272bc9474b91b7f1e56e6
YUSUF OLATUNJI <yoyforever@gmail.com>
2023-09-12 13:09:50 +0100

    things

--- app/Models/Invoice.php
+++ app/Models/Invoice.php
@@ -247,7 +247,7 @@ class Invoice extends Model
     {
         $department_id = auth()->user()->department_id;
 
-        if($department_id !== NULL) return $query;
+        if($department_id == NULL) return $query;
 
         return $query->where($this->table.'.department_id', $department_id);
     }
